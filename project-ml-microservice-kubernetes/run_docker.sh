8bd53ff00932ed2b83fe4ced9f21aafb1f80124d
Michael Sorenson <michaelscottsorenson@gmail.com>
2021-12-26 21:31:11 -0700

    Created the Dockerfile. Updated the run_docker script. Put the output of the container running in the docker_out.txt.

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -4,9 +4,12 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+sudo docker build -t project-ml:latest .
 
 # Step 2: 
 # List docker images
+sudo docker images | grep project-ml
 
 # Step 3: 
 # Run flask app
+sudo docker run -it -p 8000:80 project-ml:latest
